0f1c1897df5a07d447b27cb0e3f26737f32f182a
Nasim Dahman <nas@device-104.home>
2026-06-13 13:25:28 +0200

    LLM generates a clear and natural response

---
+++ generate.py
@@ -0,0 +1,27 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
+from retrieval import retrieve
+
+load_dotenv()
+
+def generate(question, chunks):
+    question_and_chunks = question + "\n\nContext:\n" + "\n".join(chunks)
+    
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {"role": "system", "content": "Tu es un assistant. Réponds uniquement en te basant sur les extraits fournis."},
+        {"role": "user", "content": question_and_chunks}
+        ]
+    )
+
+    print(response.choices[0].message.content)
+    return(response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    question = "Dans quelle domaine le cv fourni peut il estre utilisé ? "
+    chunks = retrieve(question, 3)
+    generate(question, chunks['documents'][0])
