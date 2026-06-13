b706525cabc8a9f280a0a1996f5d66f6ef23a738
Nas <98779137+nasdhn@users.noreply.github.com>
2026-06-13 10:25:07 +0200

    FIrst commit: files added

---
+++ ingest.py
@@ -0,0 +1,38 @@
+import fitz
+
+def parse_pdf(path):
+    result = []
+    doc = fitz.open(path)
+
+    for page in doc:
+        text = page.get_text()
+        result.append(text)
+
+    print(result[0])
+    print("-----------")
+    print("Nombre de string dasn result : ", len(result))
+    return result
+
+
+def chunk_text(pages, chunk_size=500, overlap=50):
+    result = []
+    total_text = "".join(pages)
+    i = 0
+    while (i < len(total_text)):
+        chunk = total_text[i: i + chunk_size]
+        result.append(chunk)
+        i = i + max(1, len(chunk) - overlap)
+
+    return result
+
+
+        
+
+
+if __name__ == "__main__":
+   pages =  parse_pdf("data/CV_Nasim_DAHMAN_T2i.pdf")
+   chunks = chunk_text(pages)
+   print("-----------")
+   print(len(chunks))
+   print("-----------")
+   print(chunks[1])
